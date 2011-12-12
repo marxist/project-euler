a592d84b2f6dac519c22bcf04075a3035d9d3ac3
Jure <phake.nayme@zoho.com>
2011-12-12 16:11:30 +0100

    003: initial

---
+++ 003.lisp
@@ -0,0 +1,19 @@
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;;
+;; What is the largest prime factor of the number 600851475143 ?
+
+(defparameter *number* 600851475143)
+
+(defun largest-prime (num)
+  (let ((div 2)
+        (new 0))
+    (labels ((next-prime ()
+               (setf new (/ num div))
+               (if (= (mod new 1) 0)
+                 (setf num new)
+                 (setf div (1+ div)))))
+      (loop for n from div while (< div num)
+            do (next-prime)))
+    div))
+
+(princ (largest-prime *number*))
